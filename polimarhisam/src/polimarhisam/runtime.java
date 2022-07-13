c0c46c6e9d25e2eda119a6b6d71dbf825c6451c6
poornachandu <poornachandu@cannyinfo.in>
2022-07-13 18:21:33 +0530

    uplode all

---
+++ polimarhisam/src/polimarhisam/runtime.java
@@ -0,0 +1,18 @@
+package polimarhisam;
+
+public class runtime {
+	
+	void add(int x,int y) {
+		System.out.println(x+y);
+	}
+	void add(int x,int y,int z) {
+		System.out.println(x+y+z);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 runtime h = new  runtime();
+		 h.add(30, 20,10);
+	}
+
+}
